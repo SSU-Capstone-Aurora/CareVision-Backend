1271832e0eb50ef79b5134599a5e8f491ae6b227
88dldl <chltnrgml0999@gmail.com>
2024-10-09 00:19:21 +0900

    feat: 빌더패턴 추가

--- src/main/java/aurora/carevisionapiserver/domain/camera/domain/Camera.java
+++ src/main/java/aurora/carevisionapiserver/domain/camera/domain/Camera.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import aurora.carevisionapiserver.domain.patient.domain.Patient;
 import aurora.carevisionapiserver.global.common.domain.BaseEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,7 +14,6 @@ import lombok.NoArgsConstructor;
 @Table(name = "camera")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Camera extends BaseEntity {
-
     @Id
     @Column(name = "camera_id")
     private String id;
@@ -21,4 +21,10 @@ public class Camera extends BaseEntity {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "patient_id")
     private Patient patient;
+
+    @Builder
+    public Camera(String id, Patient patient) {
+        this.id = id;
+        this.patient = patient;
+    }
 }
