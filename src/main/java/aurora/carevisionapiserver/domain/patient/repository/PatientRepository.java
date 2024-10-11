e1d5cbe17cf5910b2124d8e80c06d1f6a92d259f
88dldl <chltnrgml0999@gmail.com>
2024-10-11 23:09:52 +0900

    chore : 검색 기능 모듈화

--- src/main/java/aurora/carevisionapiserver/domain/patient/repository/PatientRepository.java
+++ src/main/java/aurora/carevisionapiserver/domain/patient/repository/PatientRepository.java
@@ -1,13 +1,9 @@
 package aurora.carevisionapiserver.domain.patient.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import aurora.carevisionapiserver.domain.patient.domain.Patient;
+import aurora.carevisionapiserver.global.common.repository.SearchRepository;
 
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-    @Query(value = "SELECT * FROM patient p WHERE p.name LIKE %?1%", nativeQuery = true)
-    List<Patient> searchPatient(String patientName);
-}
+public interface PatientRepository
+        extends JpaRepository<Patient, Long>, SearchRepository<Patient> {}
