dd64349e597cde1f0cd47ea68aff9164bbef0d8d
yel-m <coin6442@gmail.com>
2024-10-18 00:56:03 +0900

    refactor : static import로 변경

--- src/main/java/aurora/carevisionapiserver/domain/admin/converter/AdminConverter.java
+++ src/main/java/aurora/carevisionapiserver/domain/admin/converter/AdminConverter.java
@@ -1,13 +1,14 @@
 package aurora.carevisionapiserver.domain.admin.converter;
 
 import aurora.carevisionapiserver.domain.admin.domain.Admin;
-import aurora.carevisionapiserver.domain.admin.dto.AdminDTO;
 import aurora.carevisionapiserver.domain.hospital.domain.Hospital;
+
+import static aurora.carevisionapiserver.domain.admin.dto.AdminDTO.*;
 
 public class AdminConverter {
 
     public static Admin toAdmin(
-            AdminDTO.AdminJoinDTO adminJoinDTO, String password, Hospital hospital) {
+            AdminJoinDTO adminJoinDTO, String password, Hospital hospital) {
         return Admin.builder()
                 .username(adminJoinDTO.getUsername())
                 .password(password)
@@ -15,8 +16,8 @@ public class AdminConverter {
                 .build();
     }
 
-    public static AdminDTO.AdminInfoDTO toAdminInfoDTO(Admin admin, Hospital hospital) {
-        return AdminDTO.AdminInfoDTO.builder()
+    public static AdminInfoDTO toAdminInfoDTO(Admin admin, Hospital hospital) {
+        return AdminInfoDTO.builder()
                 .id(admin.getId())
                 .hospital(hospital.getName())
                 .department(hospital.getDepartment())
