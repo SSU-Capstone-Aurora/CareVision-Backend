af3239a958e8c1a4589c027bb7b53c8656c152b5
yel-m <coin6442@gmail.com>
2024-10-18 00:42:36 +0900

    refactor : 필요없는 @Repository 애너테이션 삭제

--- src/main/java/aurora/carevisionapiserver/domain/hospital/repository/HospitalRepository.java
+++ src/main/java/aurora/carevisionapiserver/domain/hospital/repository/HospitalRepository.java
@@ -1,9 +1,7 @@
 package aurora.carevisionapiserver.domain.hospital.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import aurora.carevisionapiserver.domain.hospital.domain.Hospital;
 
-@Repository
 public interface HospitalRepository extends JpaRepository<Hospital, Long> {}
