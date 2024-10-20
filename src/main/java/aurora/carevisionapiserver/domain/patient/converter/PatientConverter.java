9d006ec27c67132782893c3937f6c61d0eeb6802
88dldl <chltnrgml0999@gmail.com>
2024-10-20 19:45:40 +0900

    feat: 환자 정보 DTO로 응답

--- src/main/java/aurora/carevisionapiserver/domain/patient/converter/PatientConverter.java
+++ src/main/java/aurora/carevisionapiserver/domain/patient/converter/PatientConverter.java
@@ -5,6 +5,8 @@ import static aurora.carevisionapiserver.domain.patient.dto.response.PatientResp
 import java.util.List;
 
 import aurora.carevisionapiserver.domain.patient.domain.Patient;
+import aurora.carevisionapiserver.domain.patient.dto.response.PatientResponse.PatientProfileDTO;
+import aurora.carevisionapiserver.domain.patient.dto.response.PatientResponse.PatientProfileDTOList;
 import aurora.carevisionapiserver.domain.patient.dto.response.PatientResponse.PatientSearchListResponse;
 
 public class PatientConverter {
@@ -24,6 +26,26 @@ public class PatientConverter {
         return PatientSearchListResponse.builder()
                 .patientList(
                         patients.stream().map(PatientConverter::toPatientSearchResponse).toList())
+                .count(patients.size())
+                .build();
+    }
+
+    public static PatientProfileDTO toPatientProfileDTO(Patient patient) {
+        return PatientProfileDTO.builder()
+                .name(patient.getName())
+                .inpatientWardNumber(
+                        patient.getBed() != null ? patient.getBed().getInpatientWardNumber() : null)
+                .patientRoomNumber(
+                        patient.getBed() != null ? patient.getBed().getPatientRoomNumber() : null)
+                .bedNumber(patient.getBed() != null ? patient.getBed().getBedNumber() : null)
+                .code(patient.getCode())
+                .createdAt(patient.getCreatedAt().toLocalDate())
+                .build();
+    }
+
+    public static PatientProfileDTOList toPatientProfileDTOList(List<Patient> patients) {
+        return PatientProfileDTOList.builder()
+                .patients(patients.stream().map(PatientConverter::toPatientProfileDTO).toList())
                 .count(patients.size())
                 .build();
     }
--- src/main/java/aurora/carevisionapiserver/domain/patient/dto/response/PatientResponse.java
+++ src/main/java/aurora/carevisionapiserver/domain/patient/dto/response/PatientResponse.java
@@ -1,5 +1,6 @@
 package aurora.carevisionapiserver.domain.patient.dto.response;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import lombok.Builder;
@@ -20,6 +21,24 @@ public class PatientResponse {
     @Getter
     public static class PatientSearchListResponse {
         private List<PatientSearchResponse> patientList;
+        private int count;
+    }
+
+    @Builder
+    @Getter
+    public static class PatientProfileDTO {
+        private String name;
+        private String code;
+        private LocalDate createdAt;
+        private Long inpatientWardNumber;
+        private Long patientRoomNumber;
+        private Long bedNumber;
+    }
+
+    @Builder
+    @Getter
+    public static class PatientProfileDTOList {
+        private List<PatientProfileDTO> patients;
         private int count;
     }
 }
