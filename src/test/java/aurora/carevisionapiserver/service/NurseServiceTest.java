059e193891f38dc65b9b61641e54baf5080a1da8
yel-m <coin6442@gmail.com>
2024-10-20 13:17:04 +0900

    test : 간호사 회원가입 관련 테스트 코드 추가

--- src/test/java/aurora/carevisionapiserver/service/NurseServiceTest.java
+++ src/test/java/aurora/carevisionapiserver/service/NurseServiceTest.java
@@ -2,7 +2,9 @@ package aurora.carevisionapiserver.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -16,9 +18,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import aurora.carevisionapiserver.domain.hospital.domain.Hospital;
+import aurora.carevisionapiserver.domain.nurse.converter.NurseConverter;
 import aurora.carevisionapiserver.domain.nurse.domain.Nurse;
+import aurora.carevisionapiserver.domain.nurse.dto.request.NurseRequest.NurseCreateRequest;
 import aurora.carevisionapiserver.domain.nurse.repository.NurseRepository;
 import aurora.carevisionapiserver.domain.nurse.service.Impl.NurseServiceImpl;
 
@@ -26,6 +31,8 @@ import aurora.carevisionapiserver.domain.nurse.service.Impl.NurseServiceImpl;
 public class NurseServiceTest {
     @InjectMocks private NurseServiceImpl nurseService;
     @Mock private NurseRepository nurseRepository;
+
+    @Mock private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     private Nurse createNurse() {
         Hospital hospital = Hospital.builder().id(1L).name("서울병원").department("성형외과").build();
@@ -55,6 +62,36 @@ public class NurseServiceTest {
                 .registeredAt(LocalDateTime.parse(dateTime, formatter))
                 .hospital(hospital)
                 .build();
+    }
+
+    @Test
+    @DisplayName("간호사 회원가입에 성공한다.")
+    void createNurseSuccess() {
+        // Given
+        NurseCreateRequest nurseCreateRequest =
+                NurseCreateRequest.builder()
+                        .username("nurse1")
+                        .password("password123")
+                        .name("오로라")
+                        .build();
+
+        Hospital hospital = Hospital.builder().id(1L).name("오로라 병원").department("정형외과").build();
+
+        String encryptedPassword = "encryptedPassword123";
+        Nurse expectedNurse =
+                NurseConverter.toNurse(nurseCreateRequest, encryptedPassword, hospital);
+
+        // When
+        when(bCryptPasswordEncoder.encode(nurseCreateRequest.getPassword()))
+                .thenReturn(encryptedPassword);
+        when(nurseRepository.save(any(Nurse.class))).thenReturn(expectedNurse);
+
+        Nurse resultNurse = nurseService.createNurse(nurseCreateRequest, hospital);
+
+        // Then
+        assertEquals(expectedNurse.getUsername(), resultNurse.getUsername());
+        assertEquals(encryptedPassword, resultNurse.getPassword());
+        assertEquals(expectedNurse.getHospital(), resultNurse.getHospital());
     }
 
     @Test
