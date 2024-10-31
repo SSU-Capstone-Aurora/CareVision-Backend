3d6206b7b85d76918be1ff72e2dab174ae84a92a
yel-m <coin6442@gmail.com>
2024-10-31 23:35:10 +0900

    refactor : 시간 기준 값 상수화

--- src/main/java/aurora/carevisionapiserver/global/util/TimeAgoUtil.java
+++ src/main/java/aurora/carevisionapiserver/global/util/TimeAgoUtil.java
@@ -6,6 +6,11 @@ import java.time.Period;
 
 public class TimeAgoUtil {
 
+    private static final int MINUTES_IN_HOUR = 60;
+    private static final int HOURS_IN_DAY = 24;
+    private static final int DAYS_IN_MONTH = 30;
+    private static final int MONTHS_IN_YEAR = 12;
+
     public static String getTimeAgoMessage(LocalDateTime requestTime) {
         LocalDateTime now = LocalDateTime.now();
         Duration duration = Duration.between(requestTime, now);
@@ -13,14 +18,14 @@ public class TimeAgoUtil {
 
         if (duration.toMinutes() < 1) {
             return "방금 전";
-        } else if (duration.toMinutes() < 60) {
+        } else if (duration.toMinutes() < MINUTES_IN_HOUR) {
             return duration.toMinutes() + "분 전";
-        } else if (duration.toHours() < 24) {
+        } else if (duration.toHours() < HOURS_IN_DAY) {
             return duration.toHours() + "시간 전";
-        } else if (period.getDays() < 30) {
+        } else if (period.getDays() < DAYS_IN_MONTH) {
             return period.getDays() + "일 전";
-        } else if (period.getMonths() < 12) {
-            int months = period.getMonths() + period.getYears() * 12;
+        } else if (period.getMonths() < MONTHS_IN_YEAR) {
+            int months = period.getMonths() + period.getYears() * MONTHS_IN_YEAR;
             return months + "개월 전";
         } else {
             return period.getYears() + "년 전";
