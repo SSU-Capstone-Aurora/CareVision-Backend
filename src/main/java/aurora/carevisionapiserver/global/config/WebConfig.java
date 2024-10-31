85fe33dab9bd05683470a2ca19494b1abb045dfc
88dldl <chltnrgml0999@gmail.com>
2024-11-01 00:19:33 +0900

    feat: 사용자 인증을 위한 ArgumentResolver WebConfig에 추가

--- src/main/java/aurora/carevisionapiserver/global/config/WebConfig.java
+++ src/main/java/aurora/carevisionapiserver/global/config/WebConfig.java
@@ -1,14 +1,19 @@
 package aurora.carevisionapiserver.global.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import aurora.carevisionapiserver.global.util.validation.resolver.AuthUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final AuthUserArgumentResolver authUserArgumentResolver;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -18,5 +23,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(false)
                 .maxAge(6000);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authUserArgumentResolver);
     }
 }
