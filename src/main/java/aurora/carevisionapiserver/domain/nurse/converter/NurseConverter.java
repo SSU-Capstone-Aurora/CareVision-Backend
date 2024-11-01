aca506d0925873ed1249c1093ab327e9f3d4e282
yel-m <coin6442@gmail.com>
2024-11-02 04:45:50 +0900

    refactor : 등록 요청 리스폰스 컨버터 이름에 Register를 붙이도록 수정

--- src/main/java/aurora/carevisionapiserver/domain/nurse/converter/NurseConverter.java
+++ src/main/java/aurora/carevisionapiserver/domain/nurse/converter/NurseConverter.java
@@ -56,13 +56,13 @@ public class NurseConverter {
     public static NurseRegisterRequestListResponse toNurseRegisterRequestListResponse(
             List<Nurse> nurseList) {
         List<NurseRegisterRequestInfoResponse> nurseRequestInfoList =
-                nurseList.stream().map(NurseConverter::toNurseRequestInfoResponse).toList();
+                nurseList.stream().map(NurseConverter::toNurseRegisterRequestInfoResponse).toList();
 
         return new NurseRegisterRequestListResponse(
                 nurseRequestInfoList.size(), nurseRequestInfoList);
     }
 
-    public static NurseRegisterRequestInfoResponse toNurseRequestInfoResponse(Nurse nurse) {
+    public static NurseRegisterRequestInfoResponse toNurseRegisterRequestInfoResponse(Nurse nurse) {
         String timeAgoMessage = TimeAgoUtil.getTimeAgoMessage(nurse.getCreatedAt());
         return NurseRegisterRequestInfoResponse.builder()
                 .nurseId(nurse.getId())
