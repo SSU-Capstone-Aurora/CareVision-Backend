10d5d33880d9bef5d1b8e3696035f9d0bf8ac718
88dldl <chltnrgml0999@gmail.com>
2024-11-01 15:34:38 +0900

    chore: 스웨거 조회시 맨 아래에 나타내기 위해 태그 추가

--- src/main/java/aurora/carevisionapiserver/global/infra/aws/HealthCheckController.java
+++ src/main/java/aurora/carevisionapiserver/global/infra/aws/HealthCheckController.java
@@ -1,9 +1,11 @@
 package aurora.carevisionapiserver.global.infra.aws;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "X_HealthCheck 🏥", description = "서버 상태 체크 API")
 public class HealthCheckController {
     @GetMapping("/health")
     public String healthCheck() {
