dbeb7ad396fd489f38d44387c42daa07c535113b
88dldl <chltnrgml0999@gmail.com>
2024-11-01 16:29:52 +0900

    chore: 코드 컨벤션 적용

--- src/main/java/aurora/carevisionapiserver/global/infra/aws/HealthCheckController.java
+++ src/main/java/aurora/carevisionapiserver/global/infra/aws/HealthCheckController.java
@@ -1,8 +1,9 @@
 package aurora.carevisionapiserver.global.infra.aws;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @Tag(name = "X_HealthCheck 🏥", description = "서버 상태 체크 API")
