4deea5321ef7379d03204171626c35cb55c8ecca
yel-m <coin6442@gmail.com>
2024-11-07 17:52:32 +0900

    fix : ErrorStatus 추가 및 용도에 맞게 Status Code 수정

--- src/main/java/aurora/carevisionapiserver/global/error/code/status/ErrorStatus.java
+++ src/main/java/aurora/carevisionapiserver/global/error/code/status/ErrorStatus.java
@@ -29,10 +29,13 @@ public enum ErrorStatus implements BaseErrorCode {
     USERNAME_DUPLICATED(HttpStatus.BAD_REQUEST, "AUTH400", "이미 사용 중인 아이디입니다."),
     REFRESH_TOKEN_NULL(HttpStatus.BAD_REQUEST, "AUTH401", "refresh token이 null입니다."),
     INVALID_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, "AUTH402", "refresh token이 인식되지 않았습니다."),
-    REFRESH_TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "AUTH403", "refresh token이 만료되었습니다."),
-    INVALID_CREDENTIALS(HttpStatus.BAD_REQUEST, "AUTH404", "인증에 실패하였습니다."),
+    REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH403", "refresh token이 만료되었습니다."),
+    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "AUTH404", "인증에 실패하였습니다."),
     USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH405", "해당하는 유저 이름을 찾을 수 없습니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH406", "해당하는 유저를 찾을 수 없습니다."),
+    USER_NOT_ACTIVATED(HttpStatus.FORBIDDEN, "AUTH407", "승인되지 않은 유저입니다."),
+    INVALID_ROLE(HttpStatus.FORBIDDEN, "AUTH408", "유효하지 않은 권한입니다."),
+
     // Admin
     ADMIN_NOT_FOUND(HttpStatus.NOT_FOUND, "ADMIN400", "관리자를 찾을 수 없습니다."),
     ;
