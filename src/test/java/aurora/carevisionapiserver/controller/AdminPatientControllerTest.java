8f142ca511f19c47965b7ef9c94c51bab9e4d2fd
yel-m <coin6442@gmail.com>
2024-11-10 19:39:33 +0900

    test : 관리자의 환자 등록 API 컨트롤러 테스트 코드 추가

--- src/test/java/aurora/carevisionapiserver/controller/AdminPatientControllerTest.java
+++ src/test/java/aurora/carevisionapiserver/controller/AdminPatientControllerTest.java
@@ -1,6 +1,9 @@
 package aurora.carevisionapiserver.controller;
 
+import static aurora.carevisionapiserver.util.PatientUtil.createPatient;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -16,45 +19,38 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import aurora.carevisionapiserver.domain.bed.domain.Bed;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import aurora.carevisionapiserver.domain.admin.service.AdminService;
+import aurora.carevisionapiserver.domain.camera.dto.request.CameraRequest.CameraSelectRequest;
+import aurora.carevisionapiserver.domain.camera.service.CameraService;
+import aurora.carevisionapiserver.domain.nurse.service.NurseService;
 import aurora.carevisionapiserver.domain.patient.api.AdminPatientController;
 import aurora.carevisionapiserver.domain.patient.domain.Patient;
+import aurora.carevisionapiserver.domain.patient.dto.request.PatientRequest.PatientCreateRequest;
+import aurora.carevisionapiserver.domain.patient.dto.request.PatientRequest.PatientRegisterRequest;
 import aurora.carevisionapiserver.domain.patient.service.PatientService;
+import aurora.carevisionapiserver.global.auth.util.JWTUtil;
+import aurora.carevisionapiserver.util.CameraUtils;
+import aurora.carevisionapiserver.util.PatientUtil;
 
 @WebMvcTest(AdminPatientController.class)
 class AdminPatientControllerTest {
 
     @MockBean private PatientService patientService;
+    @MockBean private CameraService cameraService;
+    @MockBean private AdminService adminService;
+    @MockBean private NurseService nurseService;
+    @MockBean private JWTUtil jwtUtil;
     @Autowired private MockMvc mockMvc;
-
-    private Patient createPatient() {
-        Bed bed =
-                Bed.builder()
-                        .id(1L)
-                        .inpatientWardNumber(1L)
-                        .patientRoomNumber(2L)
-                        .bedNumber(3L)
-                        .build();
-        return Patient.builder().id(1L).name("test").code("kk-123").bed(bed).build();
-    }
-
-    private Patient createOtherPatient() {
-        Bed bed =
-                Bed.builder()
-                        .id(2L)
-                        .inpatientWardNumber(2L)
-                        .patientRoomNumber(3L)
-                        .bedNumber(3L)
-                        .build();
-        return Patient.builder().id(2L).name("kangrok").code("rr-123").bed(bed).build();
-    }
+    @Autowired private ObjectMapper objectMapper;
 
     @Test
     @WithMockUser
     @DisplayName("환자를 검색에 성공한다.")
     void searchPatientSuccess() throws Exception {
         String patientName = "test";
-        List<Patient> patients = List.of(createPatient(), createOtherPatient());
+        List<Patient> patients = List.of(createPatient(), PatientUtil.createOtherPatient());
         given(patientService.searchPatient(patientName)).willReturn(patients);
 
         mockMvc.perform(
@@ -70,5 +66,26 @@ class AdminPatientControllerTest {
                 .andExpect(jsonPath("$.result.patientList[1].patientName").value("kangrok"))
                 .andExpect(jsonPath("$.result.patientList[1].inpatientWardNumber").value(2))
                 .andExpect(jsonPath("$.result.patientList[1].code").value("rr-123"));
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("환자 등록에 성공한다.")
+    void createPatientSuccess() throws Exception {
+        PatientCreateRequest patientCreateRequest = PatientUtil.createPatientCreateRequest();
+        CameraSelectRequest cameraSelectRequest = CameraUtils.createCameraSelectRequest();
+        PatientRegisterRequest patientRegisterRequest =
+                new PatientRegisterRequest(patientCreateRequest, cameraSelectRequest);
+
+        given(patientService.createPatient(patientCreateRequest)).willReturn(createPatient());
+
+        mockMvc.perform(
+                        post("/api/admin/patients")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(patientRegisterRequest))
+                                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("COMMON201"))
+                .andExpect(jsonPath("$.message").value("요청 성공 및 리소스 생성됨"));
     }
 }
