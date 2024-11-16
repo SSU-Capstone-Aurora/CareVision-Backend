4752c98c5880a9229375456eecaf64b055cc781a
88dldl <chltnrgml0999@gmail.com>
2024-11-17 02:57:36 +0900

    feat: username으로 토큰을 찾는 기능 구현

--- src/main/java/aurora/carevisionapiserver/global/fcm/repository/ClientTokenRepository.java
+++ src/main/java/aurora/carevisionapiserver/global/fcm/repository/ClientTokenRepository.java
@@ -1,7 +1,11 @@
 package aurora.carevisionapiserver.global.fcm.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import aurora.carevisionapiserver.global.fcm.domain.ClientToken;
 
-public interface ClientTokenRepository extends CrudRepository<ClientToken, String> {}
+public interface ClientTokenRepository extends CrudRepository<ClientToken, String> {
+    Optional<String> findClientTokenByUsername(String username);
+}
