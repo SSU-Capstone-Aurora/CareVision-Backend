4addb0359b2be6396b6f485b8449658f613d1bfb
yel-m <coin6442@gmail.com>
2024-11-26 14:10:12 +0900

    style : 코드 포맷팅 적용

--- src/main/java/aurora/carevisionapiserver/domain/camera/api/NurseCameraController.java
+++ src/main/java/aurora/carevisionapiserver/domain/camera/api/NurseCameraController.java
@@ -2,9 +2,6 @@ package aurora.carevisionapiserver.domain.camera.api;
 
 import java.util.List;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +14,10 @@ import aurora.carevisionapiserver.domain.nurse.domain.Nurse;
 import aurora.carevisionapiserver.global.error.BaseResponse;
 import aurora.carevisionapiserver.global.error.code.status.SuccessStatus;
 import aurora.carevisionapiserver.global.security.handler.annotation.AuthUser;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -31,7 +31,7 @@ public class NurseCameraController {
 
     @Operation(summary = "환자와 연결되지 않은 카메라 목록 조회 API", description = "환자와 연결되지 않은 카메라 목록 조회합니다_예림")
     @ApiResponses({
-            @ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+        @ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
     })
     @GetMapping("/unlinked")
     public BaseResponse<CameraInfoListResponse> getUnlinkedCameras(
