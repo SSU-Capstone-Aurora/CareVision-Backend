a93ef6b6ab10197ffe773629529db64ff3e1b242
yel-m <coin6442@gmail.com>
2024-11-26 21:38:21 +0900

    fix : 연결되지 않은 카메라 조회 API를 Admin, Nurse가 모두 사용할 수 있도록 수정 및 컨트롤러 분리

--- src/main/java/aurora/carevisionapiserver/domain/camera/api/CameraController.java
+++ src/main/java/aurora/carevisionapiserver/domain/camera/api/CameraController.java
@@ -1,22 +1,16 @@
 package aurora.carevisionapiserver.domain.camera.api;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import aurora.carevisionapiserver.domain.camera.converter.CameraConverter;
 import aurora.carevisionapiserver.domain.camera.domain.Camera;
 import aurora.carevisionapiserver.domain.camera.dto.response.CameraResponse;
-import aurora.carevisionapiserver.domain.camera.dto.response.CameraResponse.StreamingInfoResponse;
-import aurora.carevisionapiserver.domain.camera.dto.response.CameraResponse.StreamingListResponse;
 import aurora.carevisionapiserver.domain.camera.service.CameraService;
-import aurora.carevisionapiserver.domain.nurse.domain.Nurse;
-import aurora.carevisionapiserver.domain.patient.domain.Patient;
-import aurora.carevisionapiserver.domain.patient.service.PatientService;
+import aurora.carevisionapiserver.global.auth.domain.User;
 import aurora.carevisionapiserver.global.response.BaseResponse;
 import aurora.carevisionapiserver.global.response.code.status.SuccessStatus;
 import aurora.carevisionapiserver.global.security.handler.annotation.AuthUser;
@@ -27,43 +21,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
-@Tag(name = "Camera 📷", description = "카메라 관련 API")
+@Tag(name = "Common - Camera 📷", description = "공통 - 카메라 관련 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/cameras")
 public class CameraController {
     private final CameraService cameraService;
-    private final PatientService patientService;
-
-    @Operation(summary = "특정 환자 실시간 영상 스트리밍 관련 정보 조회 API", description = "환자의 스트리밍 관련 정보를 조회합니다_숙희")
-    @ApiResponses({
-        @ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
-        @ApiResponse(responseCode = "CAMERA400", description = "NOT FOUND, 카메라를 찾을 수 없습니다.")
-    })
-    @GetMapping("/streaming/{patientId}")
-    public BaseResponse<StreamingInfoResponse> getStreamingInfo(
-            @PathVariable(name = "patientId") Long patientId) {
-        Patient patient = patientService.getPatient(patientId);
-        String cameraUrl = cameraService.getStreamingUrl(patient);
-        return BaseResponse.of(
-                SuccessStatus._OK, CameraConverter.toStreamingInfoResponse(cameraUrl, patient));
-    }
-
-    @Operation(
-            summary = "전체 담당 환자 실시간 영상 스트리밍 관련 정보 조회 API",
-            description = "담당하는 환자의 전체 스트리밍 관련 정보를 조회합니다_숙희")
-    @ApiResponses({
-        @ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
-        @ApiResponse(responseCode = "CAMERA400", description = "NOT FOUND, 카메라를 찾을 수 없습니다.")
-    })
-    @GetMapping("/streaming")
-    public BaseResponse<StreamingListResponse> getStreamingInfoList(
-            @Parameter(name = "nurse", hidden = true) @AuthUser Nurse nurse) {
-        List<Patient> patients = patientService.getPatients(nurse);
-        Map<Patient, String> streamingInfo = cameraService.getStreamingInfo(patients);
-        return BaseResponse.of(
-                SuccessStatus._OK, CameraConverter.toStreamingListResponse(streamingInfo));
-    }
 
     @Operation(summary = "환자와 연결되지 않은 카메라 목록 조회 API", description = "환자와 연결되지 않은 카메라 목록 조회합니다_예림")
     @ApiResponses({
@@ -71,8 +34,8 @@ public class CameraController {
     })
     @GetMapping("/unlinked")
     public BaseResponse<CameraResponse.CameraInfoListResponse> getUnlinkedCameras(
-            @Parameter(name = "nurse", hidden = true) @AuthUser Nurse nurse) {
-        List<Camera> cameras = cameraService.getCameraInfoUnlinkedToPatient(nurse);
+            @Parameter(name = "user", hidden = true) @AuthUser User user) {
+        List<Camera> cameras = cameraService.getCameraInfoUnlinkedToPatient(user);
         return BaseResponse.of(
                 SuccessStatus._OK, CameraConverter.toCameraInfoListResponse(cameras));
     }
---
+++ src/main/java/aurora/carevisionapiserver/domain/camera/api/NurseCameraController.java
@@ -0,0 +1,65 @@
+package aurora.carevisionapiserver.domain.camera.api;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import aurora.carevisionapiserver.domain.camera.converter.CameraConverter;
+import aurora.carevisionapiserver.domain.camera.dto.response.CameraResponse.StreamingInfoResponse;
+import aurora.carevisionapiserver.domain.camera.dto.response.CameraResponse.StreamingListResponse;
+import aurora.carevisionapiserver.domain.camera.service.CameraService;
+import aurora.carevisionapiserver.domain.nurse.domain.Nurse;
+import aurora.carevisionapiserver.domain.patient.domain.Patient;
+import aurora.carevisionapiserver.domain.patient.service.PatientService;
+import aurora.carevisionapiserver.global.response.BaseResponse;
+import aurora.carevisionapiserver.global.response.code.status.SuccessStatus;
+import aurora.carevisionapiserver.global.security.handler.annotation.AuthUser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "Camera - Nurse 📷", description = "간호사 - 카메라 관련 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class NurseCameraController {
+    private final CameraService cameraService;
+    private final PatientService patientService;
+
+    @Operation(summary = "특정 환자 실시간 영상 스트리밍 관련 정보 조회 API", description = "환자의 스트리밍 관련 정보를 조회합니다_숙희")
+    @ApiResponses({
+        @ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+        @ApiResponse(responseCode = "CAMERA400", description = "NOT FOUND, 카메라를 찾을 수 없습니다.")
+    })
+    @GetMapping("/streaming/{patientId}")
+    public BaseResponse<StreamingInfoResponse> getStreamingInfo(
+            @PathVariable(name = "patientId") Long patientId) {
+        Patient patient = patientService.getPatient(patientId);
+        String cameraUrl = cameraService.getStreamingUrl(patient);
+        return BaseResponse.of(
+                SuccessStatus._OK, CameraConverter.toStreamingInfoResponse(cameraUrl, patient));
+    }
+
+    @Operation(
+            summary = "전체 담당 환자 실시간 영상 스트리밍 관련 정보 조회 API",
+            description = "담당하는 환자의 전체 스트리밍 관련 정보를 조회합니다_숙희")
+    @ApiResponses({
+        @ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+        @ApiResponse(responseCode = "CAMERA400", description = "NOT FOUND, 카메라를 찾을 수 없습니다.")
+    })
+    @GetMapping("/streaming")
+    public BaseResponse<StreamingListResponse> getStreamingInfoList(
+            @Parameter(name = "nurse", hidden = true) @AuthUser Nurse nurse) {
+        List<Patient> patients = patientService.getPatients(nurse);
+        Map<Patient, String> streamingInfo = cameraService.getStreamingInfo(patients);
+        return BaseResponse.of(
+                SuccessStatus._OK, CameraConverter.toStreamingListResponse(streamingInfo));
+    }
+}
