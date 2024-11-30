7b76d9ab91014d0b4611742f01f676ef52a71b66
88dldl <chltnrgml0999@gmail.com>
2024-11-30 18:59:25 +0900

    refactor: 썸네일 요청시 최근에 저장된 이미지 응답하는 방식으로 수정

--- src/main/java/aurora/carevisionapiserver/domain/camera/service/Impl/CameraServiceImpl.java
+++ src/main/java/aurora/carevisionapiserver/domain/camera/service/Impl/CameraServiceImpl.java
@@ -1,17 +1,13 @@
 package aurora.carevisionapiserver.domain.camera.service.Impl;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import jakarta.transaction.Transactional;
 
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import aurora.carevisionapiserver.domain.admin.domain.Admin;
 import aurora.carevisionapiserver.domain.camera.domain.Camera;
@@ -22,7 +18,6 @@ import aurora.carevisionapiserver.domain.patient.exception.CameraException;
 import aurora.carevisionapiserver.global.auth.domain.User;
 import aurora.carevisionapiserver.global.auth.service.S3Service;
 import aurora.carevisionapiserver.global.response.code.status.ErrorStatus;
-import aurora.carevisionapiserver.global.util.UriFormatter;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -30,9 +25,7 @@ import lombok.RequiredArgsConstructor;
 public class CameraServiceImpl implements CameraService {
     private static final int CAMERA_IP_INDEX = 0;
     private static final int CAMERA_PW_INDEX = 1;
-    private static final String S3_KEY = "s3_url";
     private final S3Service s3Service;
-    private final UriFormatter uriFormatter;
 
     @Value("${camera.streaming.url}")
     String urlFormat;
@@ -74,22 +67,8 @@ public class CameraServiceImpl implements CameraService {
     }
 
     private String getThumbnail(Patient patient) {
-        String rtspUrl = getStreamingUrl(patient);
         Long patientId = patient.getId();
-        URI requestUrl = uriFormatter.getThumbnailUrl(rtspUrl, patientId.toString());
-        if (requestUrl == null) {
-            return s3Service.getRecentImage(patientId);
-        }
-
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity(requestUrl, String.class);
-
-        if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
-            JSONObject jsonResponse = new JSONObject(response.getBody());
-            return jsonResponse.getString(S3_KEY);
-        } else {
-            return s3Service.getRecentImage(patientId);
-        }
+        return s3Service.getRecentImage(patientId);
     }
 
     private List<String> getCameraInfoLinkedToPatient(Patient patient) {
