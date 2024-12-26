9921db5031283794a405785e502212a7126a78ea
88dldl <chltnrgml0999@gmail.com>
2024-12-26 17:52:26 +0900

    chore: 알람 요청/응답 DTO를 기준으로 폴더 구조 리팩토링

--- src/main/java/aurora/carevisionapiserver/global/fcm/dto/response/AlarmPreviewResponse.java
+++ src/main/java/aurora/carevisionapiserver/global/fcm/dto/response/AlarmPreviewResponse.java
@@ -1,4 +1,4 @@
-package aurora.carevisionapiserver.global.fcm.dto;
+package aurora.carevisionapiserver.global.fcm.dto.response;
 
 public record AlarmPreviewResponse(long count) {
     public static AlarmPreviewResponse of(long count) {
