428a4fe28be4cdf5e98a804580dc6a0783a74664
88dldl <chltnrgml0999@gmail.com>
2025-02-11 15:26:56 +0900

    refactor: 컨트롤러에서의 회원가입 로직을 서비스로 위임

--- src/main/java/aurora/carevisionapiserver/domain/admin/service/AdminService.java
+++ src/main/java/aurora/carevisionapiserver/domain/admin/service/AdminService.java
@@ -1,11 +1,10 @@
 package aurora.carevisionapiserver.domain.admin.service;
 
 import aurora.carevisionapiserver.domain.admin.domain.Admin;
-import aurora.carevisionapiserver.domain.admin.dto.request.AdminRequest.AdminCreateRequest;
-import aurora.carevisionapiserver.domain.hospital.domain.Department;
+import aurora.carevisionapiserver.domain.admin.dto.request.AdminRequest.AdminSignUpRequest;
 
 public interface AdminService {
-    Admin createAdmin(AdminCreateRequest adminCreateRequest, Department department);
+    Admin signup(AdminSignUpRequest adminSignUpRequest);
 
     Admin getAdmin(Long adminId);
 
