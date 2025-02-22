a78cd4f6193a690f48202b902c84bb9cc111e1f5
yel-m <coin6442@gmail.com>
2025-02-22 15:20:19 +0900

    test : 연결되지 않은 환자 목록을 검색을 위한 컨트롤러 테스트 추가

---
+++ src/test/java/aurora/carevisionapiserver/domain/nurse/api/NurseControllerTest.java
@@ -0,0 +1,46 @@
+package aurora.carevisionapiserver.domain.nurse.api;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import aurora.carevisionapiserver.ControllerTestSupport;
+import aurora.carevisionapiserver.domain.bed.domain.Bed;
+import aurora.carevisionapiserver.domain.patient.domain.PatientDocument;
+import aurora.carevisionapiserver.global.response.code.status.SuccessStatus;
+
+class NurseControllerTest extends ControllerTestSupport {
+
+    @WithMockUser
+    @DisplayName("간호사와 연결되지 않은 환자 목록을 검색합니다.")
+    @Test
+    void searchUnlinkedPatientList() throws Exception {
+        // given
+        Map<PatientDocument, Bed> patientDocumentBedMap = new HashMap<>();
+
+        // when
+        when(patientService.searchUnlinkedPatients(any())).thenReturn(patientDocumentBedMap);
+
+        // then
+        mockMvc.perform(
+                        get("/api/patients/unlinked/search")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .with(csrf())
+                                .param("search", "환자명"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(SuccessStatus._OK.getCode()))
+                .andExpect(jsonPath("$.result.patientList").isArray())
+                .andExpect(jsonPath("$.result.count").isNumber());
+    }
+}
