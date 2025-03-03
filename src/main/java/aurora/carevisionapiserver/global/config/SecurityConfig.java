d6d0417c46247383f9b8640cd356639ff9607627
yel-m <coin6442@gmail.com>
2025-03-03 16:39:22 +0900

    feat : 환자 및 간호사 데이터 Elasticsearch 동기화 API 구현

--- src/main/java/aurora/carevisionapiserver/global/config/SecurityConfig.java
+++ src/main/java/aurora/carevisionapiserver/global/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
         "/swagger-ui/**",
         "/swagger-resources/**",
         "/v3/api-docs/**",
+        "/api/internal/**",
     };
 
     private final String[] nurseUrls = {
---
+++ src/main/java/aurora/carevisionapiserver/global/elasticsearch/service/ElasticSearchSyncService.java
@@ -0,0 +1,8 @@
+package aurora.carevisionapiserver.global.elasticsearch.service;
+
+public interface ElasticSearchSyncService {
+
+    void syncPatientDatabaseToElasticsearch();
+
+    void syncNurseDatabaseToElasticsearch();
+}
