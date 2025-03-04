9d7eb4dec5c9db8b847092235e2023948134ffb1
88dldl <chltnrgml0999@gmail.com>
2025-03-04 17:25:24 +0900

    test: 담당 환자 조회 페이지네이션 통합 테스트 추가

---
+++ src/test/java/aurora/carevisionapiserver/domain/nurse/api/NurseControllerIntegrationTest.java
@@ -0,0 +1,117 @@
+package aurora.carevisionapiserver.domain.nurse.api;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import aurora.carevisionapiserver.IntegrationTestSupport;
+import aurora.carevisionapiserver.domain.bed.domain.Bed;
+import aurora.carevisionapiserver.domain.bed.repository.BedRepository;
+import aurora.carevisionapiserver.domain.hospital.domain.Department;
+import aurora.carevisionapiserver.domain.hospital.domain.Hospital;
+import aurora.carevisionapiserver.domain.hospital.repository.DepartmentRepository;
+import aurora.carevisionapiserver.domain.hospital.repository.HospitalRepository;
+import aurora.carevisionapiserver.domain.nurse.domain.Nurse;
+import aurora.carevisionapiserver.domain.nurse.repository.NurseRepository;
+import aurora.carevisionapiserver.domain.patient.domain.Patient;
+import aurora.carevisionapiserver.domain.patient.repository.PatientRepository;
+import aurora.carevisionapiserver.domain.patient.service.PatientService;
+import aurora.carevisionapiserver.global.common.service.PageService;
+import aurora.carevisionapiserver.global.response.code.status.SuccessStatus;
+
+@AutoConfigureMockMvc
+public class NurseControllerIntegrationTest extends IntegrationTestSupport {
+    @Autowired MockMvc mockMvc;
+    @Autowired PatientRepository patientRepository;
+    @Autowired DepartmentRepository departmentRepository;
+    @Autowired HospitalRepository hospitalRepository;
+    @Autowired NurseRepository nurseRepository;
+    @Autowired BedRepository bedRepository;
+    @MockBean PatientService patientService;
+    @MockBean PageService pageService;
+
+    @WithMockUser(
+            username = "nurse",
+            roles = {"NURSE"})
+    @DisplayName("담당하는 환자 내역을 조회한다.")
+    @Test
+    void getPatientList() throws Exception {
+        // given
+        Department department = createDepartment();
+        Nurse nurse = createNurse(department);
+        Bed bed = createBed(department);
+
+        Patient patient = createPatient(1L, department, nurse, bed);
+        Slice<Patient> patientSlice = new SliceImpl<>(List.of(patient));
+
+        // when
+        when(patientService.getPatientSlice(any(Nurse.class), anyLong(), anyInt()))
+                .thenReturn(patientSlice);
+        when(pageService.getNextCursor(anyInt(), any())).thenReturn(1L);
+
+        // then
+        mockMvc.perform(
+                        get("/api/patients")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .with(csrf())
+                                .param("lastIdx", "0")
+                                .param("size", "2"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(SuccessStatus._OK.getCode()))
+                .andExpect(jsonPath("$.result.patients").isArray())
+                .andExpect(jsonPath("$.result.hasNext").value(false))
+                .andExpect(jsonPath("$.result.nextCursor").value(1L));
+    }
+
+    private Patient createPatient(Long id, Department department, Nurse nurse, Bed bed) {
+        Patient patient =
+                Patient.builder().id(id).bed(bed).department(department).nurse(nurse).build();
+        return patientRepository.save(patient);
+    }
+
+    private Department createDepartment() {
+        Hospital hospital = createHospital();
+        Department department = Department.builder().name("department").hospital(hospital).build();
+        return departmentRepository.save(department);
+    }
+
+    private Hospital createHospital() {
+        Hospital hospital = Hospital.builder().name("hospital").build();
+        return hospitalRepository.save(hospital);
+    }
+
+    private Bed createBed(Department department) {
+        Bed bed =
+                Bed.builder()
+                        .bedNumber(1L)
+                        .patientRoomNumber(2L)
+                        .inpatientWardNumber(3L)
+                        .department(department)
+                        .build();
+        return bedRepository.save(bed);
+    }
+
+    private Nurse createNurse(Department department) {
+        Nurse nurse =
+                Nurse.builder().department(department).username("nurse").isActivated(true).build();
+        return nurseRepository.save(nurse);
+    }
+}
