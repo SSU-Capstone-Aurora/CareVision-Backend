24f9919db660d891a604685481dc08e613f65e4f
yel-m <coin6442@gmail.com>
2025-03-21 01:49:59 +0900

    fix : 간호사 이름 검색 시 name.ngram이 아닌 name으로 검색하도록 수정

--- src/main/java/aurora/carevisionapiserver/domain/patient/repository/PatientEsRepository.java
+++ src/main/java/aurora/carevisionapiserver/domain/patient/repository/PatientEsRepository.java
@@ -8,13 +8,13 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import aurora.carevisionapiserver.domain.patient.domain.PatientDocument;
 
 public interface PatientEsRepository extends ElasticsearchRepository<PatientDocument, String> {
-    @Query("{\"match\": {\"name.ngram\": \"?0\"}}")
+    @Query("{\"match\": {\"name\": \"?0\"}}") // TODO : 이 메서드 사용하는 API 통합할 때 ES 쿼리 메서드로 변경
     List<PatientDocument> searchByName(String name);
 
     @Query("{\"bool\": {\"must\": [ {\"term\": {\"nurseId\": -1}} ] }}")
     List<PatientDocument> findAllByNurseIdIsNull();
 
     @Query(
-            "{\"bool\": {\"must\": [ {\"match\": {\"name.ngram\": \"?0\"}}, {\"term\": {\"nurseId\": -1}} ] }}")
+            "{\"bool\": {\"must\": [ {\"match\": {\"name\": \"?0\"}}, {\"term\": {\"nurseId\": -1}} ] }}")
     List<PatientDocument> findByNameAndNurseIdIsNull(String name);
 }
